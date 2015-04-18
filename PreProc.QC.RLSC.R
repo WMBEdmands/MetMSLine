30145f630bc4c3ec2a39437698dda2e929e07f09
Kemya <kemya@users.noreply.github.com>
2015-04-18 11:32:51 +0800

    Update PreProc.QC.RLSC.R
    na.omit gave a weird error transforming all table into 0es if no NAs were found. i made na.omit conditional to the presence of NAs

--- PreProc.QC.RLSC.R
+++ PreProc.QC.RLSC.R
@@ -32,7 +32,7 @@ PreProc.QC.RLSC<-function(Peak.picker.output.file="e.g. XCMS_output.tsv, Mzmine_
   date<-gsub("-",".",date)
   write.csv(Parameters,paste("Parameters",substr(date,1,10),".csv",sep=" "),row.names=FALSE)
   
-last.CCQC<-(grep(first.QC.name,colnames(X))+CCQC-1)
+last.CCQC<-(grep(first.QC.name,colnames(X))+CCQC)
 #peakcolumn_no<-14+SGroups
 peakcolumnsIndex<-as.numeric(1:(grep(first.QC.name,colnames(X))-1))##index of peak information columns
 columnvector<-as.numeric(last.CCQC:length(X)) #all injections numeric vector
@@ -42,7 +42,13 @@ columnvector<-as.numeric(last.CCQC:length(X)) #all injections numeric vector
 ####zero filling####
   
   ##Remove rows containing missing values
-  X<-na.omit(X)
+  #stef mod 18-apr-15
+  if(names(table(is.na(X)))[2] == TRUE){
+      X<-na.omit(X)
+    }else if (names(table(is.na(X)))[2] == NA){ 
+        message("no NAs in table to be removed")
+    }
+  
   peakcolumns<-X[,peakcolumnsIndex]###subset peak variable information columns
 
   X.2<-X #create X.2 matrix
